0af6d2f25451f3bead76845cf9f840b09df2f6ac
ajay90989 <ajay.office.patidar@gmail.com>
2025-09-24 18:43:42 +0530

    done

---
+++ backend/text.js
@@ -0,0 +1,14 @@
+// # Database Configuration
+// MONGODB_URI = mongodb + srv://ajayworkpatidar:J80YsJzW0tOXh0vE@cluster0.3vwjdgg.mongodb.net/properties
+// DATABASE_NAME = properties
+
+// # Server Configuration
+// PORT = 5000
+// NODE_ENV = development
+
+// # JWT Configuration
+// JWT_SECRET = your - super- secret - jwt - key - here - change - this -in -production
+// JWT_EXPIRE = 30d
+
+// # Client URL
+// CLIENT_URL = http://localhost:3000
